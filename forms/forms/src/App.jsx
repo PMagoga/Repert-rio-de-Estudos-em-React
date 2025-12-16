bd23dbe373b39f3b8f04c6dcb97ecddfc69b595a
PMagoga <pmagoga@gmail.com>
2025-12-15 23:14:38 -0300

    formulário em react

---
+++ forms/forms/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MyForm from "./components/Myform";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Formulário em React</h1>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
